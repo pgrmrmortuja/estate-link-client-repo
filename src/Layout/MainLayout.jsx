ea9c04bb1152a93d966d0b304cf6d0127ca200d7
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-04-04 18:41:21 +0600

    working for nav and footer difference

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -13,7 +13,7 @@ const MainLayout = () => {
 
 
     return (
-        <div className='flex flex-col '>
+        <div className='flex '>
             {noHeaderFooter || <Navbar></Navbar>}
 
             <main className=" w-11/12 mx-auto">
